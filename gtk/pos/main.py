c4a46cf2a832d36f4486cfa625aaeac1f052de77
iuri <iuri@c2e6bc37-7ee8-49e3-839a-c1d1ade10467>
2009-09-17 13:21:08 +0000

    pequenas modificações, veja #1
    git-svn-id: svn://192.168.221.129/empada/trunk@4 c2e6bc37-7ee8-49e3-839a-c1d1ade10467

--- gtk/pos/main.py
+++ gtk/pos/main.py
@@ -6,11 +6,13 @@ import gtk.glade
 
 APP="empada"
 DIR="locale"
+
 locale.setlocale(locale.LC_ALL, '')
 gtk.glade.bindtextdomain(APP, DIR)
 gtk.glade.textdomain(APP)
 gettext.bindtextdomain(APP, DIR)
 gettext.textdomain(APP)
+
 
 import pos
 
